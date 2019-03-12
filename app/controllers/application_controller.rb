0ada1ecb7ac8b604d61f60f4835ec0e6b883e9dc
rrosztoczy <rrosztoczy@gmail.com>
2019-03-12 11:26:59 -0400

    added controller skeleeton

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,38 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  #landing page
   get '/' do
+    erb :index
+  end
+
+  #who all articles
+  get '/articles' do
+    erb :index
+  end
+
+  #create article page
+  get '/articles/new' do
+    erb :new
+  end
+
+  #post new article to arcticles
+  post '/articles' do
+  end
+
+  #show specific article id
+  get '/articles/:id' do
+  end
+
+  #edit page for article to edit
+  get '/articles/:id/edit' do
+  end
+
+  #patch edits
+  patch '/articles/:id' do
+  end
+
+  #delete article
+  post '/articles/:id/delete' do
   end
 end
