b550224ee5bd827d9d2812b7ae5a6d5df19039eb
Jessy Hu <jesshuy12@gmail.com>
2019-03-12 16:51:45 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,56 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+####################################################
+#new
+  get '/articles/new' do
+    erb :new
   end
+
+#create
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+
+    redirect "/articles/#{@article.id}"
+  end
+
+##########################################################
+
+  get '/articles' do
+    #model
+    @articles = Article.all
+    #response
+    erb :index
+  end
+
+  get '/articles/:id' do
+    #model
+    @article = Article.find(params[:id])
+    #response
+    erb :show
+  end
+
+#######################################################################
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    #binding.pry
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end
+
+########################################################################
+#delete
+  delete '/articles/:id/delete' do
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+    redirect to '/articles'
+  end
+
+#######################################################################
 end
